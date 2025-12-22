2eaf805daf4f37420848e5caab73bc6a17b55734
NiranjanD0 <niranjan4281@gmail.com>
2025-12-22 06:44:52 +0530

    ignore

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -12,7 +12,7 @@ const page = () => {
         <div>
             <Snowfall color="#82C8D9" />
             <DashNav />
-            <div className='h-screen w-screen text-black flex justify-center items-center bg-[#e5e5e6] dark:bg-[#0a0a0a]'>
+            <div className='h-screen w-screen text-black flex justify-center items-center bg-[#e5e5e6] dark:bg-[#0a0a0a] dark:text-white'>
                 nigga this is dashboard
             </div>
         </div>
